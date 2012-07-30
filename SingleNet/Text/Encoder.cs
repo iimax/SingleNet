5720f67b8849203e5d3d7d3dffa58f79bf830700
iimax <admin@xfcity.org>
2012-07-30 22:22:49 +0800

    SingleNet's fitst push
    Encoder UTF8/Unicode

---
+++ SingleNet/Text/Encoder.cs
@@ -0,0 +1,50 @@
+﻿// Copyright (c) SingleNet, Inc. All rights reserved. 
+// See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Globalization;
+
+namespace SingleNet.Text
+{
+    public class Encoder
+    {
+        /// <summary>
+        /// 汉字转换为Unicode编码
+        /// </summary>
+        /// <param name="str">要编码的汉字字符串</param>
+        /// <returns>Unicode编码的的字符串</returns>
+        public static string ToUnicode(string str)
+        {
+            byte[] bts = System.Text.Encoding.Unicode.GetBytes(str);
+            string r = string.Empty;
+            for (int i = 0; i < bts.Length; i += 2)
+            {
+                r += "\\u" + bts[i + 1].ToString("x").PadLeft(2, '0') + bts[i].ToString("x").PadLeft(2, '0');
+            }
+            return r;
+        }
+
+        /// <summary>
+        /// 将Unicode编码转换为汉字字符串
+        /// </summary>
+        /// <param name="str">Unicode编码字符串</param>
+        /// <returns>汉字字符串</returns>
+        public static string ToGB2312(string str)
+        {
+            string r = string.Empty;
+            MatchCollection mc = Regex.Matches(str, @"\\u([\w]{2})([\w]{2})", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+            byte[] bts = new byte[2];
+            foreach (Match m in mc)
+            {
+                bts[0] = (byte)int.Parse(m.Groups[2].Value, NumberStyles.HexNumber);
+                bts[1] = (byte)int.Parse(m.Groups[1].Value, NumberStyles.HexNumber);
+                r += System.Text.Encoding.Unicode.GetString(bts);
+            }
+            return r;
+        }
+    }
+}
---
+++ SingleNet/Web/BasePage.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) SingleNet, Inc. All rights reserved. 
+// See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.UI;
+
+namespace SingleNet.Web
+{
+    public class BasePage : Page
+    {
+    }
+}
